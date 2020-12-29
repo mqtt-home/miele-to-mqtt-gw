594880655ebeafd7e643517f0bbee746b1949feb
Philipp Arndt <2f.mail@gmx.de>
2020-12-29 14:05:18 +0100

    debug

--- src/de.rnd7.miele/src/test/java/de/rnd7/miele/api/MieleAPITest.java
+++ src/de.rnd7.miele/src/test/java/de/rnd7/miele/api/MieleAPITest.java
@@ -10,8 +10,14 @@ public class MieleAPITest extends TestCase {
 
     private String forceEnv(String propName) {
         // macOS note: sudo vi /etc/launchd.conf
-        return Objects.requireNonNull(System.getenv(propName),
-                String.format("ENV property %s is required to run this test case", propName));
+        final String value = Objects.requireNonNull(System.getenv(propName),
+                String.format("ENV property %s is required to run this test case.", propName));
+
+        if (value.trim().isEmpty()) {
+            throw new IllegalArgumentException(String.format("ENV property %s must not be empty to run this test case.", propName));
+        }
+
+        return value;
     }
 
     private MieleAPI createAPI() {
