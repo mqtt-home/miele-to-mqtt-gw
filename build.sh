f78124c809303e3be3646fd46c63a516f272d1a5
Philipp Arndt <2f.mail@gmx.de>
2020-12-30 08:08:13 +0100

    build and push docker image (#21)
    * build and push docker image
    
    * remove digest from tag name
    
    * switch to maven shade plugin

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-set MIELE_CLIENT_ID [lindex $argv 0];
-set MIELE_CLIENT_SECRET [lindex $argv 1];
-set MIELE_PASSWORD [lindex $argv 2];
-set MIELE_USERNAME [lindex $argv 2];
+cd "$(dirname "$0")"
+cd src
 
 docker build -t pharndt/mielemqtt \
 --build-arg MIELE_CLIENT_ID=$MIELE_CLIENT_ID \
